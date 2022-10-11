2b13f0d2121bbdbee693f4c190c7761f3cd5cf40
Cue Nguyen <cue-aus@users.noreply.github.com>
2022-10-11 12:08:32 +1000

    Update emails.py

--- practicals/prac05/emails.py
+++ practicals/prac05/emails.py
@@ -8,6 +8,7 @@ def main():
     """Create dictionary of emails-to-names."""
     email_to_name = {}
     email = input("Email: ")
+    # here it is ...
     while email != "":
         name = get_name_from_email(email)
         confirmation = input(f"Is your name {name}? (Y/n) ")
